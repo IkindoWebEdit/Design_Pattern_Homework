d43da01f3e3e1aa7387b327c975de1eaa605df59
jonas <jonas.angene@gmail.com>
2021-05-18 11:16:01 +0200

    Refactoring: included POST and GET into the same function

--- Pattern_Refactoring/login/urls.py
+++ Pattern_Refactoring/login/urls.py
@@ -5,7 +5,7 @@ from . import views
 # Refactoring: used patterns instead of paths
 urlpatterns = patterns(
     'login.views',
-    url(r'^$', 'loginPage_GET'),
-    url(r'^$', 'loginPage_POST'),
+    # Refactoring: included POST and GET into the same function
+    url(r'^$', 'loginPage'),
     url(r'^logout/$', 'logoutAction')
 )
--- Pattern_Refactoring/login/views.py
+++ Pattern_Refactoring/login/views.py
@@ -6,32 +6,30 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 # Create your views here.
 
-
-def loginPage_POST(request):
+# Refactoring: included POST and GET into the same function
+def loginPage(request):
     if request.user.is_authenticated:
         return redirect('adminpage')
-    form = AuthenticationForm(request=request, data=request.POST)
-    if form.is_valid():
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
+    if request.method == "GET":
+        form = AuthenticationForm()
+        if not User.objects.filter(username='testUser').exists():
+            User.objects.create_user('testUser', 'test@user.de', 'test')
+        return render(request, 'ikindo/login_before.html', {'form': form})
+    if request.method == "POST":
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect('/adminpage/')
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/adminpage/')
+            else:
+                return HttpResponseRedirect('/login_before/')
         else:
+            messages.error(request, 'Input was invalid!')
             return HttpResponseRedirect('/login_before/')
-    else:
-        messages.error(request, 'Input was invalid!')
-        return HttpResponseRedirect('/login_before/')
-
-def loginPage_GET(request):
-    if request.user.is_authenticated:
-        return redirect('adminpage')
-    form = AuthenticationForm()
-    if not User.objects.filter(username='testUser').exists():
-        User.objects.create_user('testUser', 'test@user.de', 'test')
-    return render(request, 'ikindo/login_before.html', {'form': form})
 
 def logoutAction(request):
     logout(request)
